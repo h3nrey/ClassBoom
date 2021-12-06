0a7d22b714477986f125e3356d0b09a5c58ccd9a
henrey22 <pedro.novaes@nave.org.br>
2021-12-06 17:48:37 -0300

    bomb text done

--- Assets/Scripts/Player/BombController.cs
+++ Assets/Scripts/Player/BombController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using TMPro;
 public class BombController : MonoBehaviour
 {
     [Header("Bomb")]
@@ -11,7 +12,10 @@ public class BombController : MonoBehaviour
     bool canLauchBomb = true;
     float nextLauch = 0;
     [SerializeField] float cooldown = 3f;
+    [SerializeField] int totalOfBombs = 1;
+    private int currentBombs;
 
+    [SerializeField] TMP_Text bombText;
     private void Awake() {
         tilemap = GameObject.FindGameObjectWithTag("TilemapBrick").GetComponent<Tilemap>();
     }
@@ -19,13 +23,18 @@ public class BombController : MonoBehaviour
     private void Update() {
         tilemap = GameObject.FindGameObjectWithTag("TilemapBrick").GetComponent<Tilemap>();
         if(Time.time > nextLauch) {
-            canLauchBomb = true;            
+            canLauchBomb = true;    
+            if(currentBombs < totalOfBombs)
+            currentBombs++;        
         } else {
             canLauchBomb = false;
         }
+
+        bombText.text = currentBombs.ToString();
     }
     public void LauchBomb() {
         if (canLauchBomb) {
+            currentBombs--;
             nextLauch = Time.time + cooldown;
             Vector3Int cell = tilemap.WorldToCell(this.transform.position); //change the world position to cell position 
             Vector3 cellCenter = tilemap.GetCellCenterWorld(cell); // get the center of the cell
